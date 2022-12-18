d11da01ea7cb982f863353cd86bd3e2f4e3f32ce
Sham-Tze-Wah <shamtzewah@gmail.com>
2022-12-18 21:11:20 +0800

    Created lms

--- src/main/java/com/rbtsb/lms/LmsApplication.java
+++ src/main/java/com/rbtsb/lms/LmsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LmsApplication {
 
 	//this is the application
+	//Token : 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
 	public static void main(String[] args) {
 		SpringApplication.run(LmsApplication.class, args);
 	}
